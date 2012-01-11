5f6be7e4489792a40b69730f8a5a390e5626eda7
ajimix <ajimix@gmail.com>
2012-01-11 09:39:33 +0100

    Initial release

---
+++ check.py
@@ -0,0 +1,14 @@
+# You should install Imaging module for Python
+# http://www.pythonware.com/products/pil/
+
+from PIL import Image
+import sys, glob, os
+
+# Change here the images to check
+for file in glob.glob("*/*.jpg"):
+	try:
+		im = Image.open(file)
+		im.verify()
+	except IOError:
+		# What to do with errors. Right now just print the file name
+		print file
